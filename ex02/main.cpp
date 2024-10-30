a7f2627d5cb90df7e9802c0e90e99032b2a9c384
polvo <polvo@example.com>
2024-10-30 21:15:14 +0100

    ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,30 @@
+/* **************************************************************************** */
+/*                                                                              */
+/*                                                         :::      ::::::::    */
+/*    Makefile                                           :+:      :+:    :+:    */
+/*                                                     +:+ +:+         +:+      */
+/*    By: aiblanco <marvin@42.fr>                    +#+  +:+       +#+         */
+/*                                                 +#+#+#+#+#+   +#+            */
+/*    Created: 2024/09/20 23:05:13 by aiblanco          #+#    #+#              */
+/*    Updated: 2024/09/20 23:05:21 by aiblanco         ###   ########.fr        */
+/*                                                                              */
+/* **************************************************************************** */
+
+#include "Fixed.hpp"
+
+int	main( void )
+{
+	Fixed	a;
+	float c = 7.52;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	std::cout << a << std::endl;
+	std::cout << c << std::endl;
+	std::cout << ++c << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << Fixed::max( a, b ) << std::endl;
+	return 0;
+}
